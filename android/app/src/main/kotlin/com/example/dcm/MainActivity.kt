2b479db743316d017ac26d9023812a7d90b7bb65
leegunsun <talkcjstk@naver.com>
2023-10-28 21:33:16 +0900

    a

--- android/app/src/main/kotlin/com/example/dcm/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dcm
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/con.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get.dart';
-
-class TestController extends GetxController {
-  var findT1 = ''.obs;
-  var findT2 = ''.obs;
-  var result = ''.obs;
-  var rawResult = ''.obs;
-}
